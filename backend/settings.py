262bd32427ac7bc62d736877ecc808e81f460e2f
Shivang Dholaria <dholariashivang@gmail.com>
2024-07-18 18:57:32 -0400

    updated settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -144,10 +144,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://roulettech-assessment-frontend-bucket.s3-website-us-east-1.amazonaws.com",
-    "http://52.54.126.209"
+    "http://52.54.126.209",
+    "http://dc8f488nrq29y.cloudfront.net/"
 ]
 
 ALLOWED_HOSTS = [
     "ec2-54-91-17-67.compute-1.amazonaws.comN",
-    "52.54.126.209"
+    "52.54.126.209",
+    "http://dc8f488nrq29y.cloudfront.net/"
 ]
